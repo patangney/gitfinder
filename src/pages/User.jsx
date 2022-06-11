4fee09c0a8756d2e4af7f6424c4368573c57cc53
Patrick Tangney <ptangney@outlook.com>
2022-06-11 22:43:33 +0100

    setup user

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,15 +1,51 @@
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
 import { useEffect, useContext } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import GitHubContext from '../context/github/GithubContext'
+import Spinner from './../components/layout/Spinner'
 
 function User () {
-  const { getSingleUser, user, loading, getUserRepos, repos } = useContext(GitHubContext)
+  const { getSingleUser, user, loading, getUserRepos, repos } = useContext(
+    GitHubContext
+  )
   const params = useParams()
   useEffect(() => {
     getSingleUser(params.login)
     // getUserRepos(params.login)
   }, [])
-  return <div>User</div>
+
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable
+  } = user
+
+  if (loading) {
+    return <Spinner />
+  }
+  return (
+    <>
+      <div className='w-full mx-auto lg:w-10/12'>
+        <div className="mb-4">
+          <Link to='/' className='btn btn-ghost' >Back To Search</Link>
+        </div>
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 md:gap-8">
+          
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default User
