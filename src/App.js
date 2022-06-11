7cc4b7272b89a8d40934d44324b55548fffec4fc
Patrick Tangney <ptangney@outlook.com>
2022-06-11 14:00:29 +0100

    updated footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Navbar from './components/layout/Navbar'
 
 function App () {
   return (
