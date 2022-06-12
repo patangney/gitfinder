62f0ef615c8c7f743766a367e822dae8f93b68bf
Patrick Tangney <ptangney@outlook.com>
2022-06-12 17:53:26 +0100

    updated getUserAndRepost to sort

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -17,9 +17,13 @@ export const searchUsers = async text => {
 }
 
 export const getUserAndRepos = async login => {
+  const params = new URLSearchParams({
+    sort: 'created',
+    per_page: 10
+  })
   const [user, repos] = await Promise.all([
     gitHub.get(`/users/${login}`),
-    gitHub.get(`/users/${login}/repos`)
+    gitHub.get(`/users/${login}/repos?${params}`)
   ])
 
   return { user: user.data, repos: repos.data }
