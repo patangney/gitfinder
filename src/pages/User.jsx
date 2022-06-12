10f7e2691d9034d5ed6f4c40c4a806d3b577fb25
Patrick Tangney <ptangney@outlook.com>
2022-06-12 12:01:03 +0100

    fixed padding issue

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -63,9 +63,7 @@ function User () {
                 <img src={avatar_url} alt='' />
               </figure>
               <div className='card-body justify-end'>
-              
-                <h2 className='card-title mb-0'>{name}</h2>
-                <p className='flex-grow-0'>{login}</p>
+                <h2 className='card-title mb-0 text-slate-100'>{login}</h2>                
               </div>
             </div>
           </div>
@@ -93,36 +91,40 @@ function User () {
             </div>
 
             <div className='w-full rounded-lg shadow-md bg-base-200 stats'>
-              {location && (
-                <div className='stat'>
-                  <div className='stat-title text-md'>Location</div>
-                  <div className='text-lg stat-value'>{location}</div>
-                </div>
-              )}
-              {blog && (
-                <div className='stat'>
-                  <div className='stat-title text-md'>Website</div>
-                  <div className='text-lg stat-value'>
-                    <a href={websiteUrl} target='_blank' rel='noreferrer'>
-                      {websiteUrl}
-                    </a>
+              <div className='grid grid-cols-1 md:grid-cols-3'>
+                {location && (
+                  <div className='stat'>
+                    <div className='stat-title text-md'>Location</div>
+                    <div className='text-lg stat-value'>{location}</div>
                   </div>
-                </div>
-              )}
-              {twitter_username && (
-                <div className='stat'>
-                  <div className='stat-title text-md'>Twitter</div>
-                  <div className='text-lg stat-value'>
-                    <a
-                      href={`https://twitter.com/${twitter_username}`}
-                      target='_blank'
-                      rel='noreferrer'
-                    >
-                      {twitter_username}
-                    </a>
+                )}
+
+                {blog && (
+                  <div className='stat'>
+                    <div className='stat-title text-md'>Website</div>
+                    <div className='text-lg stat-value'>
+                      <a href={websiteUrl} target='_blank' rel='noreferrer'>
+                        {websiteUrl}
+                      </a>
+                    </div>
                   </div>
-                </div>
-              )}
+                )}
+
+                {twitter_username && (
+                  <div className='stat'>
+                    <div className='stat-title text-md'>Twitter</div>
+                    <div className='text-lg stat-value'>
+                      <a
+                        href={`https://twitter.com/${twitter_username}`}
+                        target='_blank'
+                        rel='noreferrer'
+                      >
+                        {twitter_username}
+                      </a>
+                    </div>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
